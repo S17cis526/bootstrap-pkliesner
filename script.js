00191d8ee6b96f4364cc6b9f4dabb3bdc69ce66c
Patrick Kliesner <Patrick Kliesner>
2017-03-16 15:36:56 -0500

    After Class 3/16/17

---
+++ script.js
@@ -0,0 +1,19 @@
+var displayMessage = function(message, type){
+  var html = '<div id="javascriptMessage" class="alert fade show alert-dismissable alert-' + type + '"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><span class="h4">' + message + ' </span></div>';
+  $('#message').append(html);
+  setTimeout(function() {
+  	$("#javascriptMessage").alert('close');
+  }, 3000);
+};
+
+$( document ).ready(function(){
+  $('#show_message').on('click', function(){
+    displayMessage("This is a problem!", "danger");
+  })
+
+  if($('#message_flash').length){
+    var message = $('#message_flash').val();
+    var type = $('#message_type_flash').val();
+    displayMessage(message, type);
+  }
+});
